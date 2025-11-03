3f94ef8137f277917a8eb37d9f439f1f6cbf9733
Jade1114 <jadu042596@gmail.com>
2025-11-03 10:58:31 +0800

    feat(auth): implement verification code sending endpoint

--- study-project-backend/src/main/java/org/example/service/impl/AccountServiceImpl.java
+++ study-project-backend/src/main/java/org/example/service/impl/AccountServiceImpl.java
@@ -1,16 +1,36 @@
 package org.example.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import jakarta.annotation.Resource;
 import org.example.entity.dto.Account;
 import org.example.mapper.AccountMapper;
 import org.example.service.AccountService;
+import org.example.utils.Const;
+import org.example.utils.FlowUtils;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> implements AccountService {
+
+    @Resource
+    FlowUtils flowUtils;
+
+    @Resource
+    AmqpTemplate amqpTemplate;
+
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Account account = this.findAccountByNameOrEmail(username);
@@ -24,10 +44,30 @@ public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> impl
                 .build();
     }
 
-    public Account findAccountByNameOrEmail(String text){
+    @Override
+    public String registerEmailVerifyCode(String type, String email, String ip) {
+        synchronized (ip.intern()) {
+            if (!this.verifyLimit(ip))
+                return "请求频繁，请稍后再试";
+            Random random = new Random();
+            int code = random.nextInt(899999) + 10000;
+            Map<String, Object> data = Map.of("type", type, "email", email, "ip", ip, "code", code);
+            amqpTemplate.convertAndSend("mail", data);
+            stringRedisTemplate.opsForValue()
+                    .set(Const.VERIFY_EMAIL_DATA + email, String.valueOf(code), 3, TimeUnit.MINUTES);
+            return null;
+        }
+    }
+
+    public Account findAccountByNameOrEmail(String text) {
         return this.query()
-                .eq("username",text).or()
-                .eq("email",text)
+                .eq("username", text).or()
+                .eq("email", text)
                 .one();
+    }
+
+    private boolean verifyLimit(String ip) {
+        String key = Const.VERIFY_EMAIL_LIMIT + ip;
+        return flowUtils.limitOnceCheck(key, 60);
     }
 }

21dcf0600ba5820261edcf4ddeac0080e95165b5
Jade1114 <jadu042596@gmail.com>
2025-11-03 18:37:59 +0800

    feat(auth): implement user registration endpoint

--- study-project-backend/src/main/java/org/example/service/impl/AccountServiceImpl.java
+++ study-project-backend/src/main/java/org/example/service/impl/AccountServiceImpl.java
@@ -1,8 +1,10 @@
 package org.example.service.impl;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import jakarta.annotation.Resource;
 import org.example.entity.dto.Account;
+import org.example.entity.vo.request.EmailRegisterVO;
 import org.example.mapper.AccountMapper;
 import org.example.service.AccountService;
 import org.example.utils.Const;
@@ -12,9 +14,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
+import java.util.Date;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -30,6 +33,9 @@ public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> impl
 
     @Resource
     StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -50,7 +56,7 @@ public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> impl
             if (!this.verifyLimit(ip))
                 return "请求频繁，请稍后再试";
             Random random = new Random();
-            int code = random.nextInt(899999) + 10000;
+            int code = random.nextInt(900000) + 10000;
             Map<String, Object> data = Map.of("type", type, "email", email, "ip", ip, "code", code);
             amqpTemplate.convertAndSend("mail", data);
             stringRedisTemplate.opsForValue()
@@ -59,11 +65,39 @@ public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> impl
         }
     }
 
+    @Override
+    public String registerEmailAccount(EmailRegisterVO vo) {
+        String email = vo.getEmail();
+        String username = vo.getUsername();
+        String key = Const.VERIFY_EMAIL_DATA + email;
+        String code = stringRedisTemplate.opsForValue().get(key);
+        if (code == null) return "请先获取验证码";
+        if (!code.equals(vo.getCode())) return "验证码输入错误，请重新输入";
+        if (this.existsAccountByEmail(email)) return "此电子邮件已被其他用户注册";
+        if (this.existsAccountbByUsername(username)) return "此用户名已被其他用户注册，请更换一个新的用户名";
+        String password = passwordEncoder.encode(vo.getPassword());
+        Account account = new Account(null, username, password, email, "user", new Date());
+        if (this.save(account)) {
+            stringRedisTemplate.delete(key);
+            return null;
+        } else {
+            return "内部错误，请联系管理员";
+        }
+    }
+
     public Account findAccountByNameOrEmail(String text) {
         return this.query()
                 .eq("username", text).or()
                 .eq("email", text)
                 .one();
+    }
+
+    private boolean existsAccountByEmail(String email) {
+        return this.baseMapper.exists(Wrappers.<Account>query().eq("email", email));
+    }
+
+    private boolean existsAccountbByUsername(String username) {
+        return this.baseMapper.exists(Wrappers.<Account>query().eq("username", username));
     }
 
     private boolean verifyLimit(String ip) {
